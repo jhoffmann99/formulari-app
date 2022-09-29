fa31f7b2e9ee17c63afb9ab78f427bd87ffa8fd2
j.untch <j.untch@outlook.de>
2022-09-29 16:25:48 +0200

    initial commit

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { FooterComponent } from './components/footer/footer.component';
+
+@NgModule({
+  declarations: [NavigationComponent, FooterComponent],
+  imports: [CommonModule],
+  exports: [NavigationComponent, FooterComponent],
+})
+export class CoreModule {}
