514002495d97b7ca0e0d9d04b57dff8780e503f2
jhoffmann99 <jhoffmann91@protonmail.com>
2023-05-15 08:41:15 +0200

    npm update

--- src/app/features/pages/reset-password/reset-password.component.ts
+++ src/app/features/pages/reset-password/reset-password.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
-import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-reset-password',
