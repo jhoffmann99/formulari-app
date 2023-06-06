2f3c5a4e4475fc79054be7ad07c77834d45dad9c
jhoffmann99 <jhoffmann91@protonmail.com>
2023-06-06 06:48:26 +0200

    feat: faq, imprint components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -18,6 +18,8 @@ import { PricingComponent } from './features/pricing/pricing.component';
 import { PremiumSubscriptionComponent } from './features/subscribe/premium-subscription/premium-subscription.component';
 import { SubscriptionnComponent } from './features/subscribe/subscription/subscription.component';
 import { ContactComponent } from './features/contact/contact.component';
+import { ImprintComponent } from './features/imprint/imprint.component';
+import { FaqComponent } from './features/faq/faq.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', component: HomeComponent },
@@ -45,10 +47,12 @@ const routes: Routes = [
   },
   { path: 'subscription', component: SubscriptionnComponent },
   { path: 'subscription/plans', component: PricingComponent },
-  { path: 'subscription/premium', component: PremiumSubscriptionComponent },
+  { path: 'subscription/premium', component: PremiumSubscriptionComponent, canActivate: [AuthGuard] },
   { path: 'check/add', component: AddCheckComponent, canActivate: [AuthGuard] },
   { path: 'check/reply/:checkId', component: ReplyCheckComponent },
   { path: 'check/submitted', component: CheckSubmittedComponent },
+  { path: 'imprint', component: ImprintComponent },
+  {path: 'faq', component: FaqComponent},
   { path: '**', pathMatch: 'full', component: NotFoundComponent },
 ];
 
